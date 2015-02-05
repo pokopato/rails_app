f7fe93544c4242cc995fe4fb0377e93596f45ea2
pokopato <patora20140808@gmail.com>
2015-02-05 06:01:55 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # ページごとの完全なタイトルを返します。
+    def full_title(page_title)
+        base_title = "R on R App"
+        if page_title.empty?
+            base_title
+        else
+            "Rails | #{page_title}"
+        end
+    end
 end
